b37844d3ea0ea4b7ad96a4be092c429e9c71d662
Pedro Henrique <ped.roka2104@gmail.com>
2026-03-19 22:22:23 -0300

    Aula sobre Variaveis no Javascript

---
+++ aula01/Basico.js
@@ -0,0 +1,12 @@
+//  Javascript é uma linguagem fracamente tipada
+
+var VariavelVar = "Olá"
+let VariavelLet = 'Tudo bem?'
+const VariavelConst = 11  // constante não pode ser reatribuida
+
+console.log("Ola mundo")
+console.log(VariavelVar)
+console.log(VariavelLet)
+console.log(VariavelVar, VariavelLet) // , deu espaço
+console.log(VariavelVar + VariavelLet) // concatenação, junção das variaveis
+console.log(VariavelConst + VariavelConst)
