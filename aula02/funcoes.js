11d9a69c5420d126e23631f293ee45f38dddff1c
Pedro <ped.roka2104@gmail.com>
2026-03-26 22:14:42 -0300

    Aula sobre funções e números

---
+++ aula02/funcoes.js
@@ -0,0 +1,21 @@
+
+function somaNumeros() {
+    let valor_i = 1
+    let valor_ii = 2
+    return valor_i + valor_ii
+}
+console.log(somaNumeros())
+
+
+function somaNumerosII(valor_i, valor_ii) {
+    console.log(valor_i + valor_ii)
+}
+somaNumerosII(9, 15)
+somaNumerosII(15, 40)
+somaNumerosII(100, 50)
+
+
+const SOMA_NUMEROS_III = (valor_i, valor_ii) => {
+    return valor_i + valor_ii
+}
+console.log(SOMA_NUMEROS_III(81, 98))
---
+++ aula02/numeros.js
@@ -0,0 +1,39 @@
+
+
+let primeiro_valor = 0
+let segundo_valor = 0
+
+primeiro_valor = 9
+segundo_valor = 8
+
+
+console.log (`${primeiro_valor} + ${segundo_valor} = `, primeiro_valor + segundo_valor)
+
+console.log (`${primeiro_valor} - ${segundo_valor} = `, primeiro_valor - segundo_valor)
+
+console.log (`${primeiro_valor} / ${segundo_valor} = `, primeiro_valor / segundo_valor)
+
+console.log (`${primeiro_valor} * ${segundo_valor} = `, primeiro_valor * segundo_valor)
+
+console.log (typeof(primeiro_valor))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Snake Case é caracterizado por utilizar o underline(_)
+// Ctrl com + ou - aumenta ou diminui o zoom
+// Ctrl "" para abril terminal
+// Ctrl P pesquisar arquivos 
+// Ctrl ; para transformar em comentario
+// Shift Alt seta pra baixo para copiar e colar
+
